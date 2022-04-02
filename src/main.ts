2f17c1c465e0979d6383ce86aa97b06ff1dd4b50
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-04-02 22:03:00 +0200

    added localstorage

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import './style.css'
 
+function initData() {
+    const allBooks = getAllBooks()
+    renderBooks(allBooks)
+}
+initData()
 const dialog = document.querySelector("dialog");
+const createBookButton = document.querySelector("#createBook");
 const clearFieldsButton = document.querySelector("#clearFields")
+
 
 function changeOverlay(string: "none" | "block") {
     let overlay = document.getElementById("overlay");
@@ -11,7 +18,6 @@ function changeOverlay(string: "none" | "block") {
     overlay.style.display = string;
 }
 
-
 function clearField(inputID: string) {
     let inputElement = document.getElementById(inputID) as HTMLInputElement;
     if (inputElement === null) {
@@ -20,7 +26,6 @@ function clearField(inputID: string) {
     inputElement.value = ""
 }
 
-const createBookButton = document.querySelector("#createBook");
 
 function clearAllFields() {
     clearField("titleInput")
@@ -35,28 +40,26 @@ createBookButton?.addEventListener("click", () => {
     mainElement?.classList.add("blur");
     showOverlay()
 });
-const mainElement = document.querySelector("#overlay");
 
+
+const mainElement = document.querySelector("#overlay");
 mainElement?.addEventListener("click", () => {
     dialog?.removeAttribute("open");
     mainElement?.classList.remove("blur");
     hideOverlay()
 })
-const form = document.querySelector("form");
 
+
+const form = document.querySelector("form");
 form?.addEventListener("submit", () => {
     const formData = new FormData(form);
-
-    const bookBox = document.createElement("div");
-    const bookTemplate = document.getElementById("book-template") as HTMLTemplateElement;
-    bookBox.appendChild(bookTemplate.content.cloneNode(true))
-
-    bookBox.querySelector(".title")!.textContent = "Title: " + formData.get("title");
-    bookBox.querySelector(".author")!.textContent = "Author: " + formData.get("author");
-    bookBox.querySelector(".pages")!.textContent = "Number of Pages: " + formData.get("pages");
-    bookBox.querySelector(".status")!.textContent = "Status: " + formData.get("status");
-
-    document.getElementById("content")?.appendChild(bookBox)
+    const newBook :BookInterface = {
+        title: formData.get("title") as string,
+        author: formData.get("author") as string,
+        numberOfPages: parseInt(formData.get("pages") as string),
+        status: formData.get("status") as string
+    }
+    addAnotherBook(newBook)
     mainElement?.classList.remove("blur");
     hideOverlay()
 })
@@ -67,14 +70,57 @@ clearFieldsButton?.addEventListener("click", (e) => {
     e.preventDefault()
 })
 
+
 function showOverlay() {
     changeOverlay("block")
 }
+
 
 function hideOverlay() {
     changeOverlay("none");
 }
 
+interface BookInterface {
+    title: string
+    author: string
+    numberOfPages: number
+    status: string
+}
 
 
+function renderBooks(books: BookInterface[]) {
+    const contentContainer = document.querySelector("#content")
+    contentContainer!.innerHTML="";
+    const bookTemplate = document.querySelector("#book-template") as HTMLTemplateElement
+    books.forEach(book => {
+        const bookBox = document.createElement("div")
+        bookBox.appendChild(bookTemplate.content.cloneNode(true))
+        bookBox.querySelector(".title")!.textContent = "Title: " + book.title
+        bookBox.querySelector(".author")!.textContent = "Author: " + book.author
+        bookBox.querySelector(".pages")!.textContent = "Number of pages: " + book.numberOfPages.toString()
+        bookBox.querySelector(".status")!.textContent = "Status: " + book.status
+        contentContainer?.appendChild(bookBox)
+    })
+}
+
+
+function saveAllBooks(books: BookInterface[]) {
+    const stringifiedBooks = JSON.stringify(books);
+    localStorage.setItem("books", stringifiedBooks)
+}
+
+function getAllBooks(): BookInterface[] {
+    const stringifiedBooks = localStorage.getItem("books")
+    if (stringifiedBooks === null) {
+        return []
+    }
+    return JSON.parse(stringifiedBooks);
+}
+
+function addAnotherBook(book: BookInterface) {
+    const allBooks = getAllBooks()
+    allBooks.push(book)
+    saveAllBooks(allBooks)
+    renderBooks(allBooks)
+}
 
