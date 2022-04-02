9af24ad5c492821614925c6fd8b0b7b386893867
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-04-02 14:47:25 +0200

    added input values in template when new bookBox is created

--- src/main.ts
+++ src/main.ts
@@ -1,74 +1,78 @@
 import './style.css'
 
 const dialog = document.querySelector("dialog");
-const clearFields = document.querySelector(".clearFields")
+const clearFieldsButton = document.querySelector("#clearFields")
+
+function changeOverlay(string: "none" | "block") {
+    let overlay = document.getElementById("overlay");
+    if (overlay === null) {
+        return;
+    }
+    overlay.style.display = string;
+}
+
+
+function clearField(inputID: string) {
+    let inputElement = document.getElementById(inputID) as HTMLInputElement;
+    if (inputElement === null) {
+        return
+    }
+    inputElement.value = ""
+}
 
 const createBookButton = document.querySelector("#createBook");
+
+function clearAllFields() {
+    clearField("titleInput")
+    clearField("authorInput")
+    clearField("pagesInput")
+    clearField("statusInput")
+}
+
 createBookButton?.addEventListener("click", () => {
+    clearAllFields();
     dialog?.setAttribute("open", "");
     mainElement?.classList.add("blur");
     showOverlay()
 });
-
 const mainElement = document.querySelector("#overlay");
+
 mainElement?.addEventListener("click", () => {
     dialog?.removeAttribute("open");
     mainElement?.classList.remove("blur");
     hideOverlay()
 })
-
 const form = document.querySelector("form");
+
 form?.addEventListener("submit", () => {
     const formData = new FormData(form);
-    formData.get("titleInput");
-    console.log(formData.get("titleInput"))
-    mainElement?.classList.remove("blur");
-    hideOverlay()
-
-
 
     const bookBox = document.createElement("div");
     const bookTemplate = document.getElementById("book-template") as HTMLTemplateElement;
     bookBox.appendChild(bookTemplate.content.cloneNode(true))
 
-    document.getElementById("content")?.appendChild(bookBox)
+    bookBox.querySelector(".title")!.textContent = "Title: " + formData.get("title");
+    bookBox.querySelector(".author")!.textContent = "Author: " + formData.get("author");
+    bookBox.querySelector(".pages")!.textContent = "Number of Pages: " + formData.get("pages");
+    bookBox.querySelector(".status")!.textContent = "Status: " + formData.get("status");
 
+    document.getElementById("content")?.appendChild(bookBox)
+    mainElement?.classList.remove("blur");
+    hideOverlay()
 })
 
-clearFields?.addEventListener("click", (e) => {
-    function clearField(inputID:string) {
-        let inputElement = document.getElementById(inputID) as HTMLInputElement;
-        if (inputElement === null) {
-            return
-        }
-        inputElement.value=""
-    }
-    clearField("titleInput")
-    clearField("authorInput")
-    clearField("pagesInput")
-    clearField("statusInput")
 
+clearFieldsButton?.addEventListener("click", (e) => {
+    clearAllFields()
     e.preventDefault()
 })
 
-
-
-
 function showOverlay() {
-    let overlay = document.getElementById("overlay");
-    if (overlay === null) {
-        return;
-    }
-    overlay.style.display = "block";
-
+    changeOverlay("block")
 }
 
 function hideOverlay() {
-    let overlay = document.getElementById("overlay");
-    if (overlay === null) {
-        return;
-    }
-    overlay.style.display = "none";
+    changeOverlay("none");
 }
 
 
